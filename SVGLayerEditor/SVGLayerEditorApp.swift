adfa7a3c515ded05ac074e40aafa02f0a72271c3
Dirk Clemens <git@adcore.de>
2026-03-07 12:07:25 +0100

    Initial Commit

---
+++ SVGLayerEditor/SVGLayerEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVGLayerEditorApp.swift
+//  SVGLayerEditor
+//
+//  Created by Dirk Clemens on 07.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SVGLayerEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
